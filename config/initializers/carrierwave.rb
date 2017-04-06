e738e7959092df76d2c65442e3d02b819ce92ca5
CharlieKB <charlieforce1999@gmail.com>
2017-04-05 22:39:26 -0700

    Add sign in links

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,11 +12,23 @@
 #   config.fog_directory  = "yelpmydemo1"
 # end
 
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],                        # required
-  }
-  config.fog_directory  = ENV["FOG_DIRECTORY"]                    # required
+# CarrierWave.configure do |config|
+#   config.fog_credentials = {
+#     :provider               => 'AWS',                        # required
+#     :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                        # required
+#     :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],                        # required
+#   }
+#   config.fog_directory  = ENV["FOG_DIRECTORY"]                    # required
+# end
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
 end
