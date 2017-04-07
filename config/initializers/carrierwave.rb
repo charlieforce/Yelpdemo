a7d3f41c7f9c9ed8389761e8c53fc62e5b937d4e
CharlieKB <charlieforce1999@gmail.com>
2017-04-07 11:34:55 -0700

    displayed reviews on restaurant page

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -20,22 +20,23 @@
 #   }
 #   config.fog_directory  = ENV["FOG_DIRECTORY"]                    # required
 # end
-# if Rails.env.production?
-#   CarrierWave.configure do |config|
-#     config.fog_credentials = {
-#       # Configuration for Amazon S3
-#       :provider              => 'AWS',
-#       :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
-#       :aws_secret_access_key => ENV['AWS_SECRET_KEY'],
-#       :region                => ENV['US-WEST-2']
-#     }
-#     config.fog_directory     =  ENV['AWS_BUCKET']
-#   end
-CarrierWave.configure do |config|
- config.fog_credentials = {
-     :provider          =>    'AWS',
-     :aws_access_key_id  =>   ENV["aws_access_id"],
-     :aws_secret_access_key => ENV["aws_access_secret_key"],
-     :region => 'us-west-1'
- }
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['AWS_SECRET_KEY'],
+      :region                => ENV['US-WEST-1']
+    }
+    config.fog_directory     =  ENV['AWS_BUCKET']
+  end
 end
+# CarrierWave.configure do |config|
+#  config.fog_credentials = {
+#      :provider          =>    'AWS',
+#      :aws_access_key_id  =>   ENV["aws_access_key"],
+#      :aws_secret_access_key => ENV["aws_access_secret_key"],
+#      :region => 'us-west-1'
+#  }
+# end
