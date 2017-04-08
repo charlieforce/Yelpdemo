8e6b9d11edbbc459a1217750a0b8c1ed9aa1a9de
CharlieKB <charlieforce1999@gmail.com>
2017-04-08 10:01:30 -0700

    Add fields to Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:last_name])
+  end
+end
 end
